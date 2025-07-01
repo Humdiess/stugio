11a36d6ada028a64dda56da8154bbfb380a59456
Humdiess <hmzinchannel@gmail.com>
2025-07-01 19:17:12 +0700

    feat: auth

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
-import { clsx, type ClassValue } from "clsx"
+// utils.ts
+import { ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
